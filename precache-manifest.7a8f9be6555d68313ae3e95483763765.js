dc4ce1dc2b6e8bb01d09596c6af6f09d4cc15ea4
FachrizalTanjung <fachri_shsdua@yahoo.co.id>
2020-10-03 03:10:43 +0700

    Updates

---
+++ precache-manifest.7a8f9be6555d68313ae3e95483763765.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7c7cf9b2548881a55a01440340aa1cb",
+    "url": "/appku/index.html"
+  },
+  {
+    "revision": "a3b624af12ba2c4f97ee",
+    "url": "/appku/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c3ec0a1d7a18dfccba0e",
+    "url": "/appku/static/js/2.d125661b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/appku/static/js/2.d125661b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3b624af12ba2c4f97ee",
+    "url": "/appku/static/js/main.e820c870.chunk.js"
+  },
+  {
+    "revision": "a40894771c3581917f00",
+    "url": "/appku/static/js/runtime-main.b6654467.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/appku/static/media/logo.5d5d9eef.svg"
+  }
+]);
